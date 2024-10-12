c599ae60a5406bf099ca0b7ebda84a91a75d1449
ItsWatuyusei <ItsWatuyusei@gmail.com>
2024-10-11 21:02:23 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binance Pay - ItsWatuyusei</title>
+    <style>
+        body {
+            display: block; /* Cambiado de flex a block */
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f0f0f0;
+        }
+        .container {
+            text-align: center;
+        }
+        img {
+            max-width: 500px; /* Ajusta el tamaño según sea necesario */
+            margin: 20px 0; /* Ajustado para que el margen sea vertical */
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <a href="http://linktr.ee/ItsWatuyusei">
+            <img src="USDC.png" alt="Descripción de la imagen 1"></a>
+        <a href="http://linktr.ee/ItsWatuyusei">
+            <img src="USDT.png" alt="Descripción de la imagen 2"></a>
+    </div>
+</body>
+</html>
